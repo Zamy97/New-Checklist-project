6b59a5fa840723c981e29f036884e3b0765eb0dd
Akhtar Zaman <auz.zamy@gmail.com>
2018-10-25 00:38:02 -0700

    on slide 3

--- checklist.py
+++ checklist.py
@@ -1,7 +1,33 @@
 checklist = list()
-checklist.append('Blue')
-checklist.append('orange')
-print(checklist)
 
+# CREATE FUNCTION
 def create(item):
     checklist.append(item)
+
+# READ FUNCTION
+def read(index):
+    return checklist[index]
+
+# Update Function
+def update(index, item):
+    checklist[index]= item
+
+# DESTROY FUNCTION
+def destroy(index):
+    checklist.pop(index)
+
+# TEST FUNCTION
+def test():
+    create("purple sox")
+    create("red clock")
+
+    print(read(0))
+    print(read(1))
+
+    update(0, "pruple socks")
+    destroy(1)
+
+    print(read(0))
+    # print(read(1))
+
+test()
