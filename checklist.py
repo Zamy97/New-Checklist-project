3908cfdeba492b67ae2542ec3743989de77ffd8a
Akhtar Zaman <auz.zamy@gmail.com>
2018-10-25 11:17:54 -0700

    working on mark completed function

--- checklist.py
+++ checklist.py
@@ -10,24 +10,38 @@ def read(index):
 
 # Update Function
 def update(index, item):
-    checklist[index]= item
+    checklist[index] = item
 
 # DESTROY FUNCTION
 def destroy(index):
     checklist.pop(index)
 
+# FINDING ALL THE ITEMS FUNCTION
+def list_all_item():
+    index = 0
+    for every_item in checklist:
+        print(str(index) + every_item)
+        index += 1
+
+def mark_complected(index):
+
+
+print("\033[1;34;40m")
+
 # TEST FUNCTION
 def test():
+
     create("purple sox")
     create("red clock")
 
     print(read(0))
     print(read(1))
 
-    update(0, "pruple socks")
+    update(0, "purple socks")
     destroy(1)
 
     print(read(0))
-    # print(read(1))
+
 
 test()
+list_all_item()
